cc450fbb021eaddbb9e8e3a0cf623e333acddd7a
Nabiha Tawsen <nabihatawsen@gmail.com>
2025-05-22 13:51:01 +0600

    Add files via upload

---
+++ LARGEST NUMBER.asm
@@ -0,0 +1,50 @@
+.MODEL SMALL
+.STACK 100H
+.DATA
+    VAL1 DB ?
+    VAL2 DB ?
+    MSG DB 10,13,'ENTER TWO NUMBERS:','$'
+    MSG2 DB 10,13,'LARGEST NUMBER IS:','$'
+ 
+.CODE
+MAIN PROC 
+        MOV AX,@DATA
+        MOV DS,AX
+        MOV AH,09
+        LEA DX,MSG
+        INT 21H 
+        ;INPUT
+        MOV AH,01
+        INT 21H
+        MOV VAL1,AL
+        INT 21H
+        MOV VAL2,AL
+             
+     ;COMPARE 
+     CMP VAL1,AL
+    JGE GRATER
+     JL LESS
+     
+     
+     
+     GRATER:
+     MOV BL,VAL1
+     JMP PRINT
+     LESS:
+     MOV BL,VAL2
+     
+   PRINT:
+   MOV AH,09
+   LEA DX,MSG2
+   INT 21H
+   MOV AH,02
+   MOV DL,BL
+  
+   INT 21H 
+      
+    
+   MOV AH,4CH
+   INT 21H 
+
+   MAIN ENDP
+END MAIN
